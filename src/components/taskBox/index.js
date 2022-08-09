f1bacce2bc52b1acd9d0acbb44a10fd7baf95c9a
Clara <claradaibert@poli.ufrj.br>
2022-08-09 20:26:02 -0300

    :heavy_plus_sign: base taskBox

--- src/components/taskBox/index.js
+++ src/components/taskBox/index.js
@@ -2,8 +2,8 @@ import * as Style from './style';
 
 export default function TaskBox(){
     return(
-        <>
-        
-        </>
+        <Style.Container>
+            <Style.Header>Suas tarefas</Style.Header>
+        </Style.Container>
     )
 }
--- src/components/taskBox/style.js
+++ src/components/taskBox/style.js
@@ -1,1 +1,23 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: fit-content;
+    min-height: 300px;
+    background-color: white;
+    box-shadow: 2px 4px 20px rgba(175, 156, 203, 0.4);
+    border-radius: 10px;
+    border: none;
+    padding: 3% 4%;
+`
+
+export const Header = styled.p`
+    color: ${props => props.theme.palette.darkPurple};
+    font-size: 20px;
+    font-weight: 700;
+    font-family: "Lato", sans-serif;
+`
+
+export const TaskList = styled.div`
+    width: 100%;
+`
