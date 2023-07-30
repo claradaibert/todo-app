4c051046a88e95b7859a9284765fb6226f557ae4
clara daibert <86614787+claradaibert@users.noreply.github.com>
2023-07-30 11:46:58 -0300

    Add props to TaskBox

--- src/components/taskBox/index.js
+++ src/components/taskBox/index.js
@@ -1,6 +1,6 @@
 import * as Style from './style';
 
-export default function TaskBox(){
+export default function TaskBox({taskList, setTaskList}){
     return(
         <Style.Container>
             <Style.Header>Suas tarefas</Style.Header>
