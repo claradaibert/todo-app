5c322ba83abfa77cb92fc2f54c08283702e8c7ed
clara daibert <86614787+claradaibert@users.noreply.github.com>
2024-01-28 11:10:43 -0300

    Adjust dataBox

--- src/components/dataBox/style.js
+++ src/components/dataBox/style.js
@@ -1,44 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 100%;
-    width: 100%;
-    border-radius: 10px;
-    border: none;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background: white;
-    box-shadow: 2px 4px 10px rgba(175, 156, 203, 0.4);
-` 
+  height: 100%;
+  width: 100%;
+  border-radius: 10px;
+  border: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: white;
+  box-shadow: 2px 4px 10px rgba(175, 156, 203, 0.4);
 
-export const IconContainer = styled.div`
-    height: 50px;
-    width: 50px;
-    position: relative;
-    border-radius: 7px;
-    border: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: linear-gradient(135deg, #7F6E9A 0%, #AF9CCB 0.01%, #E1CDFE 100%);
-    color: white;
-    font-size: 30px;
-    bottom: 20px;
-`
-
-export const Title = styled.p`
+  .dataTitle {
     font-size: 20px;
-    font-family: 'Lato', sans-serif;
+    font-family: "Lato", sans-serif;
     font-weight: 700;
     position: relative;
     bottom: 15%;
-`
+  }
 
-export const Body = styled.p`
+  .dataBody {
     font-size: 13px;
-    font-family: 'Lato', sans-serif;
+    font-family: "Lato", sans-serif;
     font-weight: 400;
     position: relative;
     bottom: 10%;
-`
+  }
+`;
+
+export const IconContainer = styled.div`
+  height: 50px;
+  width: 50px;
+  position: relative;
+  border-radius: 7px;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, #7f6e9a 0%, #af9ccb 0.01%, #e1cdfe 100%);
+  color: white;
+  font-size: 30px;
+  bottom: 20px;
+`;
